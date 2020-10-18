8b5c46a819d44a5b0a78fbb3e0f99cb3fc7b8200
Eduardo Mendes <eduardo.mendes@iatec.com>
2020-10-18 19:50:40 -0300

    adição do projeto vue funcionando com o componente menu-principal

---
+++ resources/js/components/MenuPrincipal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-sm">
+            <button type="button" class="btn btn-secondary btn-lg btn-block">Saldo</button>
+        </div>
+        <div class="col-sm">
+            <button type="button" class="btn btn-secondary btn-lg btn-block">Depósito</button>
+        </div>
+        <div class="col-sm">
+            <button type="button" class="btn btn-secondary btn-lg btn-block">Saque</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Caixa Eletrônico</div>
+
+                    <div class="card-body">
+                        <div class="container">
+                            <menu-principal></menu-principal>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
